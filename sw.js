5a2debc5417a8be75322824ce91ac009ed40325e
Mainetti-Solution <mainetti.solution@gmail.com>
2026-01-03 00:32:51 +0100

    V1.1
    Testato e implementato tutte le modifiche del caso

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'ms-fly-v2.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+  // Nota: Le librerie PDF sono esterne (CDN). 
+  // Per averle offline al 100% andrebbero scaricate localmente.
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
